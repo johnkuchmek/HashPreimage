81163c079f0976b99e65e29710e09101487e5d6c
johnkuchmek <52086986+johnkuchmek@users.noreply.github.com>
2021-09-11 23:12:35 -0400

    Update hash_preimage.py

--- hash_preimage.py
+++ hash_preimage.py
@@ -8,7 +8,7 @@ def hash_preimage(target_string):
     if not all( [x in '01' for x in target_string ] ):
         print( "Input should be a string of bits" )
         return
-    nonce = b'\x00'
+    y = b'\x00'
     
     while True:
       y = os.urandom(64)
