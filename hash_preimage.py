de8edf00105ee4c4e5a7b42cd1796b5c26913466
johnkuchmek <52086986+johnkuchmek@users.noreply.github.com>
2021-09-11 23:01:20 -0400

    Update hash_preimage.py

--- hash_preimage.py
+++ hash_preimage.py
@@ -1,10 +1,20 @@
 import hashlib
 import os
+import string
+import random
 
 def hash_preimage(target_string):
+    k = 10
     if not all( [x in '01' for x in target_string ] ):
         print( "Input should be a string of bits" )
         return
     nonce = b'\x00'
+    
+    while True:
+    y = os.urandom(64)
+    wy = hashlib.sha256(y)
+    ybits = ''.join(format(byte, '08b') for byte in wy.digest())
+    if target_string[-k:] == ybits[-k:]:
+        break
 
-    return( nonce )
+    return( ybits )
